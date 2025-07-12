bd5829131a36fd0dbf14d53cc8be06bcf8db4dc7
unknown <bekamdo>
2025-07-12 16:08:09 +0300

    Created container deployment script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const {merge} = require("webpack-merge");
-const HtmlWepackPlugin = require("html-webpack-plugin");
+
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json")
